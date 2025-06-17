496bbbe7e99231b31e7e00780b5b9bdda45ca99e
fkaisin <floriankaisin@hotmail.com>
2025-06-17 16:16:22 +0200

    fixup later

--- src/schemes/token.py
+++ src/schemes/token.py
@@ -2,22 +2,22 @@ from sqlmodel import Field, SQLModel
 
 
 class TokenBase(SQLModel):
-    cg_id: str = Field(primary_key=True)
-    symbol: str = Field(index=True)
-    name: str
-    rank: int = 5000
+  cg_id: str = Field(primary_key=True)
+  symbol: str = Field(index=True)
+  name: str
+  rank: int = 5000
+  mcap: int | None = 0
+  image: str | None = None
+  price: float
+  change_1h: float = 0
+  change_24h: float = 0
+  change_7d: float = 0
+  change_30d: float = 0
+  change_1y: float = 0
 
 
-class TokenPublicSmall(TokenBase):
-    pass
-
-
-class TokenPublicBig(TokenBase):
-    mcap: int | None = 0
-    image: str | None = None
-    price: float
-    change_1h: float = 0
-    change_24h: float = 0
-    change_7d: float = 0
-    change_30d: float = 0
-    change_1y: float = 0
+class TokenPublicSmall(SQLModel):
+  cg_id: str
+  symbol: str
+  name: str
+  rank: int
---
+++ src/services/token.py
@@ -0,0 +1,23 @@
+import uuid
+
+from fastapi import HTTPException, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import load_only
+from sqlmodel import or_, select
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.db.models import Token
+
+
+class TokenService:
+  def __init__(self, session: AsyncSession):
+    self.session = session
+
+  async def search_tokens(self, search_string):
+    search = f'%{search_string}%'
+    statement = select(Token).where(
+      or_(Token.symbol.ilike(search), Token.cg_id.ilike(search), Token.name.ilike(search))  # type: ignore
+    )
+    result = await self.session.exec(statement)
+    tokens = result.all()
+
+    return tokens
